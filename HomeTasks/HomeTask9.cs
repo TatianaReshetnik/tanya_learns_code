d29bc8909c15f2cb71a9ab9c01b412c313754ea0
TatianaReshetnick <tatiana.reshetnick@tripledotstudios.com>
2024-12-27 11:00:58 +0100

    new Task Array

---
+++ HomeTasks/HomeTask9.cs
@@ -0,0 +1,14 @@
+namespace HomeTasks;
+//Create an array of integers.
+//Remove all occurrences of a specified number from the array.
+//Let the number be entered from the console. If there is no such number, output corresponding messages.
+//The result should be a new array without the specified number.
+
+internal class HomeTask9 : IBaseHomeTask
+{
+    public void Run()
+    {
+
+
+    }
+}
