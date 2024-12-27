a5160d2fb621baf0a82c9773c9a57b506787ea0a
TatianaReshetnick <tatiana.reshetnick@tripledotstudios.com>
2024-12-27 12:09:13 +0100

    no message

---
+++ HomeTasks/HomeTask10Arrays.cs
@@ -0,0 +1,12 @@
+namespace HomeTasks;
+//Create and fill an array with random numbers, then output the maximum, minimum, and average values.
+//Use the Random() method for generating random numbers. Allow the user to create an array of arbitrary size.
+//Let the size of the array be entered from the console.
+
+internal class HomeTask10Arrays : IBaseHomeTask
+{
+    public void Run()
+    {
+        
+    }
+}
