425f1c9a303598d124a9a70124fc053f6cfe4b90
TatianaReshetnick <tatiana.reshetnick@tripledotstudios.com>
2024-12-27 12:38:39 +0100

    no message

--- HomeTasks/HomeTask10Arrays.cs
+++ HomeTasks/HomeTask10Arrays.cs
@@ -7,6 +7,19 @@ internal class HomeTask10Arrays : IBaseHomeTask
 {
     public void Run()
     {
-        
+        Random rand = new Random();
+        Console.Write("How many elements do you want in your array? ");
+        int length = int.Parse(Console.ReadLine());
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++)
+            {
+                array[i] = rand.Next(1, 100);
+            }
+// Print the array and max,min, average
+            Console.WriteLine("Your array: " + string.Join(", ", array));
+            Console.WriteLine("The max value is" + "max");
+            Console.WriteLine("The min value is" + "min");
+            Console.WriteLine("The average value is" + "average");
+        }
     }
-}
